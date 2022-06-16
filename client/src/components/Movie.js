d1f9f35b391fff50f8f0e5b3af51e586d5139dd0
IDStack <idstack@outlook.co.id>
2022-06-16 21:31:59 +0700

    (client) use state and hook in functional component

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react';
 
 const Movies = () => {
-  return (
-    <h2>Choose a movie</h2>
-  )
-}
+	const [movies, setMovies] = useState([]);
 
-export default Movies
+	useEffect(() => {
+		setMovies([
+			{ id: 1, title: 'Spongebob Squarepants', runtime: 145 },
+			{ id: 2, title: 'Ogry', runtime: 145 },
+			{ id: 3, title: 'Chalk Zone', runtime: 145 },
+		]);
+	}, []);
+
+	return (
+		<Fragment>
+			<h2>Choose a movie</h2>
+			<ul>
+				{movies.map((movie) => (
+					<li key={movie.id}>{movie.title}</li>
+				))}
+			</ul>
+		</Fragment>
+	);
+};
+
+export default Movies;
