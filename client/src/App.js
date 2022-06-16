cfff6055ef9adf70c02b7d7ef0bbd95f5fe1436c
IDStack <idstack@outlook.co.id>
2022-06-16 20:51:46 +0700

    (client) implement route with react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,57 @@
+/* third party */
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
+/* style */
 import './App.css';
 
 function App() {
 	return (
-		<div className='container'>
-			<div className='row'>
-				<h1 className='mt-3'>Go React Movie Project!</h1>
-				<hr className='mb-3' />
-			</div>
-			<div className='row'>
-				<div className='col-2'>
-					<div className='card' style={{ width: '18rem' }}>
-						<ul className='list-group list-group-flush'>
-							<li className='list-group-item'>
-								<a href='/'>Home</a>
-							</li>
-							<li className='list-group-item'>
-								<a href='/movies'>Movies</a>
-							</li>
-							<li className='list-group-item'>
-                <a href="/catalogue">Catalog</a>
-              </li>
-						</ul>
+		<Router>
+			<div className='container'>
+				<div className='row'>
+					<h1 className='mt-3'>Go React Movie Project!</h1>
+					<hr className='mb-3' />
+				</div>
+				<div className='row'>
+					<div className='col-3'>
+						<div className='card'>
+							<ul className='list-group list-group-flush'>
+								<li className='list-group-item'>
+									<Link to='/'>Home</Link>
+								</li>
+								<li className='list-group-item'>
+									<Link to='/movies'>Movies</Link>
+								</li>
+								<li className='list-group-item'>
+									<Link to='/admin'>Admin</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className='col-9'>
+						<Routes>
+							<Route path='/admin' element={<Admin />} />
+							<Route path='/movies' element={<Movie />} />
+							<Route path='/' element={<Home />} />
+						</Routes>
 					</div>
 				</div>
-				<div className='col-10'></div>
 			</div>
-		</div>
+		</Router>
 	);
+}
+
+function Home() {
+	return <h1>Home</h1>;
+}
+
+function Movie() {
+	return <h1>Movies</h1>;
+}
+
+function Admin() {
+	return <h1>Admin</h1>;
 }
 
 export default App;
